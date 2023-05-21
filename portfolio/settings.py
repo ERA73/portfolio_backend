e13fa20a9c15dd3712462d59d11b6d343136f6d2
ERA73 <era-73@hotmail.com>
2023-05-21 13:48:37 -0500

    configure deploy settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 # from decouple import config
 
 DEBUG = config('DEBUG')
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", 'localhost']
 
 
 # Application definition
@@ -124,9 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 CORS_ALLOWED_ORIGINS  = config('CORS_ALLOWED_ORIGINS')
-
 CORS_ALLOW_METHODS = [
     'GET',
     'POST',
