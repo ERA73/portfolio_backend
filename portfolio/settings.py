2cac2ae220548189cef962ec1699623b334a689a
ERA73 <era-73@hotmail.com>
2023-05-22 23:03:27 -0500

    configurar hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [
+    "www.portfolio.codedevest.com", 
+    "www.codedevest.com", 
+    "127.0.0.1", 
+    "localhost"
+    ]
 
 
 # Application definition
